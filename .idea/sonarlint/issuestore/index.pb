5b06954f71d7e53d114deda20ab52119f1cf4fbf
yb12138 <1152042072@qq.com>
2021-08-18 08:45:31 +0800

    add bug

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+=
+src/demo.java,4\4\441b1311260b310596c579f521620a7d791419ae
+>
+src/demo1.java,f\d\fd8a5e25d70ac2bf3dac9ddfa037e0cf541c1151
---
+++ src/demo1.java
@@ -0,0 +1,16 @@
+public class demo1 {
+    public int circle(int height,int weight){
+        if(height>20){
+            return height*weight;
+        }else if (height<=20&&height>10){
+            height*=1.2;
+            return height*weight;
+        }else if (height>=0&&height<10){
+            height*=1.4;
+            return height*weight;
+        }
+        else {
+            return Math.abs(height)*weight;
+        }
+    }
+}
