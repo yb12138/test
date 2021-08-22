74e18dba4548c23a16c11d033ef2bc4e2a5bc7de
yb12138 <1152042072@qq.com>
2021-08-23 01:09:55 +0800

    add bug

--- src/demo1.java
+++ src/demo1.java
@@ -13,4 +13,18 @@ public class demo1 {
             return Math.abs(height)*weight;
         }
     }
+    public int circle2(int height,int weight){
+        if(height>20){
+            return height*weight;
+        }else if (height<=20&&height>10){
+            height*=1.2;
+            return height*weight;
+        }else if (height>=0&&height<10){
+            height*=1.4;
+            return height*weight;
+        }
+        else {
+            return Math.abs(height)*weight;
+        }
+    }
 }
